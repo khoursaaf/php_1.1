b95daf7a45e1ba75e5f870443fb6ddb81381b3f9
khoursaaf <khoursa@gmail.com>
2018-11-19 09:58:01 +0100

    fist commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Exercise</title>
+    </head>
+    <body>
+        <h1>Créer une variable name et l'initialiser avec la valeur de votre choix. Afficher son contenu.</h1>
+        <?php
+        $name = 'Khoursa';
+        echo 'Mon nom est ' .$name .'.'; ?>
+    </body>
+</html>
